14c19681594c2483bd41f141b8256a9a3c0cf7ac
jogt <jotjernshaugen@gmail.com>
2023-09-20 15:24:40 +0200

    Added persistent search in favorites

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -5,6 +5,7 @@ import { SongRating } from '../PlayPage';
 import FavoritesSongItem from "../../components/FavoriteSongItem";
 import {getRatedSongs, unlikeSong} from "../../utils/favoriteSongs.ts";
 import BackButton from "../../components/BackButton";
+import {useEffect, useState} from "react";
 
 export default function FavoritesPage() {
   const spotifyAPI = SpotifyAPI.getAuthorization()!;
@@ -16,15 +17,27 @@ export default function FavoritesPage() {
       enabled: getRatedSongs().filter(song => song.liked).length > 0, // Enable the query only if favoriteSongs is not empty
     }
   )
+    const [query, setQuery] = useState<string>(sessionStorage.getItem("searchQuery") || "");
+  useEffect(() => {
+    sessionStorage.setItem("searchQuery", query);
+    }, [query]);
 
   return (
     <>
       <h1>Favorites</h1>
         <BackButton/>
       <div className="song-list">
+          <input
+              type="text"
+              placeholder="Search"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+          />
         {songQuery.data &&
           songQuery.data.tracks &&
-          songQuery.data.tracks.map((track) => (
+          songQuery.data.tracks
+            .filter((track) => track.name.toLowerCase().includes(query.toLowerCase()))
+              .map((track) => (
             <FavoritesSongItem
               albumCover={track.album.images[0].url}
               title={track.name}
